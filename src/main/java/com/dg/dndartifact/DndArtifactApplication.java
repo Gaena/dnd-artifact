b691214d097e42e77835c7fe469ebfee32d47dba
gaena <ilyaz214@gmail.com>
2020-01-22 09:29:56 +0400

    Initial commit

---
+++ src/main/java/com/dg/dndartifact/DndArtifactApplication.java
@@ -0,0 +1,14 @@
+package com.dg.dndartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndArtifactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DndArtifactApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/dg/dndartifact/DndArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dg.dndartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndArtifactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
